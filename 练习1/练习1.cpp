8e7e175a8ada4ac3c6fdb494bacbd4a9da083a2c
Legolas9999 <1301676432@qq.com>
2020-09-25 16:11:31 +0800

    xin

--- 练习1/练习1.cpp
+++ 练习1/练习1.cpp
@@ -3,9 +3,10 @@
 using namespace cv;
 
 int main() {
-	//---------------------------------Á·Ï°1-------------------------------------------
+
 	cv::Mat M1 = imread("bn.jpg");
-	imshow("Ô­Í¼Ïñ",M1);
+	namedWindow("Ô­Í¼Ïñ", CV_WINDOW_NORMAL);
+	imshow("Ô­Í¼Ïñ", M1);
 	int height = M1.rows;
 	int width = M1.cols;
 	for (int j = 0; j < height; j++)
@@ -18,13 +19,15 @@ int main() {
 			M1.at<Vec3b>(j, i)[2] = average;
 		}
 	}
+	namedWindow("Æ½¾ùÍ¼Ïñ", CV_WINDOW_NORMAL);
 	imshow("Æ½¾ùÍ¼Ïñ", M1);
-	
+
 	//------------------------------Á·Ï°2--------------------------------------------
 	cv::Mat M2 = imread("bn.jpg", 0);
+	namedWindow("»Ò¶ÈÍ¼", CV_WINDOW_NORMAL);
 	imshow("»Ò¶ÈÍ¼", M2);
-	
-	
+
+
 	//------------------------Á·Ï°3------------------------------------------
 	uchar threshold = 150;
 	for (int j = 0; j < height; j++)
@@ -41,9 +44,10 @@ int main() {
 			M1.at<Vec3b>(j, i)[2] = average;
 		}
 	}
+	namedWindow("ãÐÖµÍ¼Ïñ", CV_WINDOW_NORMAL);
 	imshow("ãÐÖµÍ¼Ïñ", M1);
 
-	
+
 	//---------------------------Á·Ï°4--------------------------------------
 	cv::Mat srcMat = imread("bn.jpg");
 	cv::Mat deepMat;
@@ -65,21 +69,23 @@ int main() {
 			srcMat.at<Vec3b>(j, i)[2] = average;
 		}
 	}
+	namedWindow("Ç³¸´ÖÆ", CV_WINDOW_NORMAL);
 	imshow("Ç³¸´ÖÆ", shallowMat);
+	namedWindow("Éî¸´ÖÆ", CV_WINDOW_NORMAL);
 	imshow("Éî¸´ÖÆ", deepMat);
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	waitKey(0);
 }
